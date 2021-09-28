2e546e7903ca60f32dd4cc0d791f7ba6d916883b
palhaogv <palhaogv@gmail.com>
2021-09-28 16:51:37 +0200

    ex-002

--- day-1/ex-002.js
+++ day-1/ex-002.js
@@ -1,7 +1,14 @@
 function rockPaperScissors(f_player, s_player) {
-    if (f_player = s_player) {
-        return console.log('Draw')
+    if (f_player == s_player) {
+        return console.log('Draw');
+    } if (((f_player == 'rock') && (s_player == 'scissors')) || ((f_player == 'scissors') && (s_player == 'paper')) || ((f_player == 'paper') && (s_player == 'rock'))){
+        return console.log('First player wins');
+    } if (((s_player == 'rock') && (f_player == 'scissors')) || ((s_player == 'scissors') && (f_player == 'paper')) || ((s_player == 'paper') && (f_player == 'rock'))){
+        return console.log('Second player wins');
     }
 }
 
-rockPaperScissors(1, 1)
+rockPaperScissors('rock', 'rock')
+rockPaperScissors("rock", "paper")
+rockPaperScissors("rock", "scissors")
+rockPaperScissors("paper", "scissors")
