ddb6dae3d1f55910971a3a3068a2ccf9a137de05
palhaogv <palhaogv@gmail.com>
2021-09-28 18:26:36 +0200

    ex-007

---
+++ day-1/ex-007.js
@@ -0,0 +1,9 @@
+/*Write a function `subDigits` that takes an integer as argument, and returns it after substracting each digit to that integer.
+
+For example, if the integer is 12, it returns 12 - 1 - 2 => 9.
+
+If the integer is negative, the function returns "Argument Error".*/
+
+function subDigits(integer) {
+    console.log(integer )
+}
